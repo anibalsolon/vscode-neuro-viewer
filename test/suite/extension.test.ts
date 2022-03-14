356860dc993fb0e813255375e42fd74aa1b11799
Anibal <anibalsolon@gmail.com>
2022-03-13 23:35:57 -0500

    trying some tests

--- test/suite/extension.test.ts
+++ test/suite/extension.test.ts
@@ -1,13 +1,13 @@
 import { expect } from 'chai';
 import vscode from 'vscode';
 import path from 'path';
-// import * as extension from '../../extension';
 import * as utils from '../../webview/utils';
+// import * as extension from '../../extension';
+// import { NiftiEditorProvider } from '../../extension/viewer';
 
 suite('Extension Test Suite', () => {
   test('Sample test', async () => {
-    expect(-1).to.equal([1, 2, 3].indexOf(5));
-    expect(-1).to.equal([1, 2, 3].indexOf(0));
+    const ext = vscode.extensions.getExtension('vscode.nifti-viewer')
 
     const c1 = utils.hexToRgb('#ff0000');
     expect(c1).to.deep.equal([255, 0, 0, 255]);
@@ -15,5 +15,6 @@ suite('Extension Test Suite', () => {
     const uri = vscode.Uri.file(
       path.join(__dirname + '/../data/custom.nii')
     );
+
   });
 });
